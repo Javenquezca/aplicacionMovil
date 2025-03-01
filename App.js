6323c76e7ca3f767da5e36456efaab58ef2eba16
Javenquezca <jastivenv@gmail.com>
2025-03-01 09:12:02 -0500

    Subiendo la estructura básica del proyecto

---
+++ App.js
@@ -0,0 +1,15 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet } from "react-native";
+
+export default function App() {
+    return (
+        <View>
+            <Text>Hola desde App.js</Text>
+            <StatusBar style="auto"/>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+});
